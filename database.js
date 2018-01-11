cac1a0f9172ec05a943744f384bcbd1cac2283c2
Andrew Keller <Keller192@gmail.com>
2018-01-11 10:05:24 -0500

    adds table and syncing with database

--- database.js
+++ database.js
@@ -12,3 +12,21 @@ sequelize
     .catch(err => {
         console.error('Error: Not connected!');
     });
+
+const BlogUser = sequelize.define('blog_users', {
+    first_name: {
+        type: Sequelize.STRING
+    },
+    last_name: {
+        type: Sequelize.STRING
+    }
+});
+
+BlogUser.sync({ force: true })
+    .then(() => {
+        console.log(`Table 'blog_users' created!`);
+        return BlogUser.create({
+            first_name: 'Andrew',
+            last_name: 'Keller'
+        });
+    });
